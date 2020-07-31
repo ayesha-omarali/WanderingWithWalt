05dbb8f4d69c06e06647893797d241de0b1ab81f
Alice Bian <Alice.S.Bian@disney.com>
2020-07-31 12:11:38 -0700

    adding video, but not showing up

---
+++ components/main/TestGeoLocation.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text } from '../Themed';
+
+export default function TestGeoLocation() {
+  return (
+    <Text>TestGeoLocation</Text>
+  )
+}
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
+import { Video } from 'expo-av';
 import ParkMap from '../components/main/ParkMap'
 import EasterEgg from '../components/main/EasterEgg';
 
-export default function TabTwoScreen() {
+
+export default class App extends React.Component {
+ render() {
   return (
     <View style={styles.container}>
       <ParkMap />
+      <Video
+        source={{ uri: '../assets/images/mapvid.mp4' }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: 300, height: 300 }}
+      />
       <EasterEgg />
+
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
