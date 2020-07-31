556e638994747dce64acc6b9ca612a910005ee8e
Nguyen, Elise <Elise.Nguyen@disney.com>
2020-07-30 23:56:39 -1000

    add images

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,11 +2,23 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
 import Welcome from '../components/main/Welcome'
+<<<<<<< Updated upstream
+=======
+import {Image} from 'react-native' ;
+import { Text } from '../Themed';
+
+>>>>>>> Stashed changes
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+<<<<<<< Updated upstream
       <Welcome />
+=======
+      {/* <Welcome /> */}
+      <Image style={{ position: 'absolute', left: 10, top: 10, width: 200, height: 200 }} source={require('../assets/images/title-gold.png')}/>
+      <Image style={{ position: 'absolute', top: '40%', width: 170, height: 200 }} source={require('../assets/images/mickeynwalt-gold.png')}/>
+>>>>>>> Stashed changes
     </View>
   );
 }
